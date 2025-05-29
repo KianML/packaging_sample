6e40e0625af2cf258d92433cb30efeb7b3f43b11
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-29 10:19:38 +1000

    Update setup.py

--- my-sample-package/setup.py
+++ my-sample-package/setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup
 
+from __init__ import __version__
+
+setup(
+    name='my_sample_package',
+    version=__version__,
+
+    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    author='Michael Kim',
+    author_email='mkim0407@gmail.com',
+
+    py_modules=['my_pip_package'],
+)
