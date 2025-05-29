b62da7bf13455d45aa791e7fcbac81d78a45e63c
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-29 11:18:18 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-from my_sample_package import __version__
+#from my_sample_package import __version__
 
 setup(
     name='my_sample_package',
-    version=__version__,
+    version='1.1',
 
     url='https://github.com/kianData/Kian-Sample-Package',
     author='Kian',
