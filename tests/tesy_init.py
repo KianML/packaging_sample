f8c38a35675daa6482601dd44bdd1ddca033b0bf
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-29 10:43:08 +1000

    Update tesy_init.py

--- tests/tesy_init.py
+++ tests/tesy_init.py
@@ -1,1 +1,8 @@
+from my_sample_package import print_test
 
+
+def test_print_test(capsys):
+    print_test()
+    captured = capsys.readouterr()
+    assert captured.out == 'This is my pip package!\n'
+    assert captured.err == ''
