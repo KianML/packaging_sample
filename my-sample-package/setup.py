cf1b6aa6431b197f764b2b303c979ebd6302fcc7
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-29 10:21:36 +1000

    Update setup.py

--- my-sample-package/setup.py
+++ my-sample-package/setup.py
@@ -6,9 +6,9 @@ setup(
     name='my_sample_package',
     version=__version__,
 
-    url='https://github.com/MichaelKim0407/tutorial-pip-package',
-    author='Michael Kim',
-    author_email='mkim0407@gmail.com',
+    url='https://github.com/kianData/Kian-Sample-Package',
+    author='Kian',
+    author_email='kshvzn@gmail.com',
 
-    py_modules=['my_pip_package'],
+    py_modules=['my_sample_package'],
 )
