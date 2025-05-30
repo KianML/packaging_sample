de9154dadc964f7aec985ec714a1db2d0e148e64
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-30 12:13:47 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='sample_package_KianML',
     version=__version__,
 
-    url='https://github.com/KianML/packaging_sample',
+    url='https://github.com/KianML/packaging_sample/src',
     author='Kianoosh Keshavarzian',
     author_email='kshvzn@gmail.com',
     test_suite='tests',
