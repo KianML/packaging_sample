04393453a4c3d8e236462d2f8906943426080963
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-30 12:20:47 +1000

    Delete tests/test_init.py

--- tests/test_init.py
+++
@@ -1,7 +0,0 @@
-from main import print_msg
-
-def test_print_msg(capsys):
-    print_msg()
-    captured = capsys.readouterr()
-    assert captured.out == 'This is my pip package!\n'
-    assert captured.err == ''
