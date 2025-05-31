9b99091b211cfde19f5d786a5cc454b66453c425
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-05-31 14:22:25 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-#from src.sample_package_KianML import __version__
+from src.sample_package_KianML import __version__
 
 setup(
     name='sample_package_KianML',
-    version='1.5',
+    version=__version__,
 
     url='https://github.com/KianML/packaging_sample',
     author='Kianoosh Keshavarzian',
